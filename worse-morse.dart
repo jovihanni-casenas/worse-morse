d4c090bfa643a83e5970678f78c9f84eb947fd8a
jovihanni-casenas <92214389+jovihanni-casenas@users.noreply.github.com>
2022-05-02 19:42:37 +0800

    Worse morse dart source code for activity 5 of subject PEC 02.

---
+++ worse-morse.dart
@@ -0,0 +1,32 @@
+import 'dart:io';
+import 'dart:core';
+
+var morse = [".-", "-...", "-.-.", "-..", ".", "..-.",
+             "--.", "....", "..", ".---", "-.-", ".-..",
+             "--", "-.", "---", ".--.", "--.-", ".-.",
+             "...", "-", "..-", "...-", ".--", "-..-",
+             "-.--", "--.."];
+var alphabet = ["A", "B", "C", "D", "E", "F",
+                "G", "H", "I", "J", "K", "L",
+                "M", "N", "O", "P", "Q", "R",
+                "S", "T", "U", "V", "W", "X",
+                "Y", "Z"];
+
+void main(){
+    var str = stdin.readLineSync();
+    morse_decode(str, "");
+}
+
+void morse_decode(var toDecode, var decoded){
+  if(toDecode.length == 0)
+    print(decoded);
+  else{
+    for(int i=0;i<26;i++){
+      if(toDecode.startsWith(morse[i])){
+        var currentDecoded = decoded + alphabet[i];
+        var currentToDecode = toDecode.substring(morse[i].length);
+        morse_decode(currentToDecode, currentDecoded);
+      }
+    }
+  }
+}
